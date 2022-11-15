3b37e2e00d53b30019bce556075fb210c44e8ba2
nazifcand <nazifcandurgutt@gmail.com>
2022-11-15 03:44:25 +0300

    added google analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-css-tags */
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,20 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="/fontawesome5/css/all.min.css" />
         <link rel="icon" type="image/png" href="/favicon.png" />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y389N6FTVC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y389N6FTVC');
+        `}
+        </Script>
       </Head>
 
       <body>
