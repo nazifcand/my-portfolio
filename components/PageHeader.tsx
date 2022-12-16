be038797f755fc91df7d1bf7579e59334162acd9
nazifcand <nazifcandurgutt@gmail.com>
2022-12-16 19:06:44 +0300

    fix pageheade props

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,4 +1,11 @@
-const PageHeader = ({ title, children }) => {
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  title: string;
+  children?: ReactNode;
+}
+
+const PageHeader: FC<Props> = ({ title, children }) => {
   return (
     <div id="page-header">
       <div id="page-title">
